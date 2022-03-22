1e7e099f0c783ad250a51ec5c2b3644eddcb059a
MetroRug <74714365+MetroRug@users.noreply.github.com>
2022-03-22 20:44:12 -0300

    correção de erros

--- BLOGCURSO/categorias/admin.py
+++ BLOGCURSO/categorias/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Categoria
 
+
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome_cat')
     list_display_links = ('id', 'nome_cat')
